f380ce0f2b0b2653220be9a68d8d1f0873625f44
Murilo Henrique <34652210+murilothink@users.noreply.github.com>
2020-05-20 13:47:59 -0300

    Add files via upload

---
+++ src/main/java/com/springsba/teste/demo/SpringBootAdminServerApplication.java
@@ -0,0 +1,18 @@
+package com.springsba.teste.demo;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@EnableAdminServer
+@SpringBootApplication
+public class SpringBootAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminServerApplication.class, args);
+	}
+
+}
